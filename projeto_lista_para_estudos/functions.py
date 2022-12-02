d17269236f2d683127aee172288e6e24926d88ac
victorcaixetaf <victorcaixetaf@gmail.com>
2022-12-01 21:13:50 -0300

    debug

--- projeto_lista_para_estudos/functions.py
+++ projeto_lista_para_estudos/functions.py
@@ -1,17 +1,16 @@
 import random
 from lists import listx
 
-y = input('How many exercices do you want? ')
-
 try:
+    y = int(input('How many exercices do you want? '))
     if (y != int(y)) or (y <= 0) or (y == '') or (y >= key.listx):
-        print('Thanks!!!!')
+        print('Invalid Number!')
         print()
 except:
     print('Write only int Numbers!!!!!')
     print()
 finally:
-    y = int(y)
+    exit()
     
 def randon_list(x):
     rand_dict_list = {key: random.sample(range(value), y) for key, value in x().items()}
